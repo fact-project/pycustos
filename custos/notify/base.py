712d997a1029d7590806781948dfd66d8088f592
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2016-07-05 19:37:10 +0200

    Notifiers listen to all categories if categories is None

--- custos/notify/base.py
+++ custos/notify/base.py
@@ -21,25 +21,26 @@ class Notifier(metaclass=ABCMeta):
 
     Recipients can either be an iterable or a dict mapping categories to recipients
     '''
-    def __init__(self, recipients, level=INFO, categories=('default', )):
+    def __init__(self, recipients, level=INFO, categories=None):
         self.recipients = recipients
         self.level = level
-        self.categories = set(categories)
+        self.categories = set(categories) if categories else None
 
     def handle_message(self, msg):
-        if msg.category in self.categories and msg.level >= self.level:
+        if msg.level >= self.level:
+            if self.categories is None or msg.category in self.categories:
 
-            if isinstance(self.recipients, Mapping):
-                log.debug('Recipients is a Mapping')
-                recipients = self.recipients[msg.category]
-            else:
-                recipients = self.recipients
+                if isinstance(self.recipients, Mapping):
+                    log.debug('Recipients is a Mapping')
+                    recipients = self.recipients[msg.category]
+                else:
+                    recipients = self.recipients
 
-            for recipient in recipients:
-                try:
-                    self.notify(recipient, msg)
-                except:
-                    log.exception('Could not notifiy recipient {}'.format(recipient))
+                for recipient in recipients:
+                    try:
+                        self.notify(recipient, msg)
+                    except:
+                        log.exception('Could not notifiy recipient {}'.format(recipient))
 
     @abstractmethod
     def notify(self, recipient, msg):
