3c2f02474614186d7c7abcaa43a9b748ae6c7d60
Kai Bruegge <kai.bruegge@tu-dortmund.de>
2016-09-01 15:44:40 +0200

    use proper logging level

--- custos/notify/console.py
+++ custos/notify/console.py
@@ -7,4 +7,13 @@ log = logging.getLogger(__name__)
 
 class ConsoleNotifier(Notifier):
     def notify(self, recipient, msg):
-        log.info('Message: {}    For: {}'.format(msg.text, recipient))
+        if msg.level == logging.INFO:
+            log.info('Message: {}    For: {}'.format(msg.text, recipient))
+        if msg.level == logging.DEBUG:
+            log.debug('Message: {}    For: {}'.format(msg.text, recipient))
+        if msg.level == logging.WARN:
+            log.warn('Message: {}    For: {}'.format(msg.text, recipient))
+        if msg.level == logging.CRITICAL:
+            log.critical('Message: {}    For: {}'.format(msg.text, recipient))
+        if msg.level == logging.ERROR:
+            log.error('Message: {}    For: {}'.format(msg.text, recipient))
