56f3d3833745abe4916e6cac7d5733a76d821861
Kai Bruegge <kai.bruegge@tu-dortmund.de>
2016-09-02 09:47:53 +0200

    add a test case for checking thread lifecycle

--- tests/custos_test.py
+++ tests/custos_test.py
@@ -1,9 +1,13 @@
-from custos import Custos, Message, IntervalCheck
-import threading
+from custos import Custos, Message, IntervalCheck, ConsoleNotifier, levels
 from time import sleep
+import logging
 
+log = logging.getLogger('custos')
+log.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+log.addHandler(handler)
 
-class TestCheck(IntervalCheck):
+class MockCheck(IntervalCheck):
     ''' This check just sends a message each time its run. '''
 
     def check(self):
@@ -15,23 +19,33 @@ class TestCheck(IntervalCheck):
         )
 
 def test_stop_flag():
+    test_check = MockCheck(interval=1)
 
-    test_check = TestCheck(interval=3)
+    console = ConsoleNotifier(
+        level=levels.INFO,
+        recipients=['test_1', 'test_2'],
+    )
 
     custos = Custos(
         checks=[test_check],
-        notifiers=[],
+        notifiers=[console],
     )
-    print("asdasdasd")
-    custos.run()
-    sleep(500)
-    # try:
-    #     while True:
-    #         sleep(10)
-    # except (SystemExit, KeyboardInterrupt):
-    #     custos.stop()
-    #
+
+    assert custos.is_alive() == False
+    custos.start()
+
+    assert custos.is_alive() == True
+
+    log.debug('All Checks runnig')
+    log.debug('Sleeping in main thread for 2 seconds')
+    sleep(2)
+    log.debug('Sleep finished')
+
     custos.stop()
+    sleep(1)
+    assert custos.stop_event.is_set()
+    sleep(2)
+    assert custos.is_alive() == False
 
 if __name__ == '__main__':
     test_stop_flag()
